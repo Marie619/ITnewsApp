2a934c3200ba8de38de78048f20813f008182d27
Marie619 <umairmajeed719@gmail.com>
2020-03-19 12:50:58 +0500

    posting news module done

--- ItNewsApp/app/src/main/java/app/muneef/itnewsapp/adapters/NewsApapter.java
+++ ItNewsApp/app/src/main/java/app/muneef/itnewsapp/adapters/NewsApapter.java
@@ -1,5 +1,9 @@
 package app.muneef.itnewsapp.adapters;
 
+import android.content.Context;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
@@ -9,32 +13,60 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.bumptech.glide.Glide;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.List;
+
 import app.muneef.itnewsapp.R;
+import app.muneef.itnewsapp.models.News;
+import app.muneef.itnewsapp.models.Users;
 
 public class NewsApapter extends RecyclerView.Adapter<NewsApapter.NewsViewHolder> {
+
+    private List<News> newsList;
+    private Context context;
+
+
+    private DatabaseReference mNewsDbRef;
+    private ValueEventListener mGetLikersListListener;
+
+    public NewsApapter(List<News> newsList, Context context) {
+        this.newsList = newsList;
+        this.context = context;
+        mNewsDbRef = FirebaseDatabase.getInstance().getReference().child("News");
+    }
 
     @NonNull
     @Override
     public NewsViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_item_news, parent, false);
+        return new NewsViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull NewsViewHolder holder, int position) {
 
+        holder.bind(newsList.get(position));
+
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return newsList.size();
     }
 
     public class NewsViewHolder extends RecyclerView.ViewHolder {
 
-        ImageView imgProfilePoster,imgNews;
-        TextView txtNews,txtTotalLikes,txtProfileName;
+        ImageView imgProfilePoster, imgNews;
+        TextView txtNews, txtTotalLikes, txtProfileName;
         Button btnLike;
-
 
 
         public NewsViewHolder(@NonNull View itemView) {
@@ -47,5 +79,84 @@ public class NewsApapter extends RecyclerView.Adapter<NewsApapter.NewsViewHolder
             txtTotalLikes = itemView.findViewById(R.id.txt_total_likes);
             btnLike = itemView.findViewById(R.id.btn_like);
         }
+
+        void bind(final News news) {
+            txtNews.setText(news.getPostText());
+
+            DatabaseReference mUsersDbRef = FirebaseDatabase.getInstance().getReference().child("Users");
+
+            ValueEventListener mGetUser = new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                    Users user = dataSnapshot.getValue(Users.class);
+
+                    if (user.getProfileImageUrl() != null) {
+
+                        Glide.with(imgProfilePoster.getContext()).load(user.getProfileImageUrl()).into(imgProfilePoster);
+                    }
+                    txtProfileName.setText(user.getUserName());
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                }
+            };
+            mUsersDbRef.child(news.getPosterId()).addValueEventListener(mGetUser);
+
+
+            //TODO: now work for checking already liked or not
+
+            String currentUid = FirebaseAuth.getInstance().getCurrentUser().getUid();
+
+            if (news.getLikedBy() != null) {
+
+                if (news.getLikedBy().containsValue(currentUid)) {
+                    Drawable img = context.getResources().getDrawable(R.drawable.ic_thumb_up_blue_24dp);
+                    btnLike.setCompoundDrawablesWithIntrinsicBounds(img, null, null, null);
+                    btnLike.setText("Liked");
+                    btnLike.setTextColor(context.getResources().getColor(R.color.iconLikeColor));
+                    btnLike.setEnabled(false);
+                } else {
+                    Drawable img = context.getResources().getDrawable(R.drawable.ic_thumb_up_grey_24dp);
+                    btnLike.setCompoundDrawablesWithIntrinsicBounds(img, null, null, null);
+                    btnLike.setText("Like");
+                    btnLike.setTextColor(Color.BLACK);
+                }
+            }
+
+
+            // Here the checking for likes is finished
+
+            String totalLikes = String.valueOf(news.getTotalLikes());
+            txtTotalLikes.setText(totalLikes);
+
+            if (news.getPostImage() != null) {
+                imgNews.setVisibility(View.VISIBLE);
+                Glide.with(imgNews.getContext()).load(news.getPostImage()).into(imgNews);
+            }
+
+
+            btnLike.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    //TODO: CHANGE icon and update total likes
+                    String cUid = FirebaseAuth.getInstance().getCurrentUser().getUid();
+                    mNewsDbRef.child(news.getPostId()).child("likedBy").push().setValue(cUid);
+
+                    int totalLikes = news.getTotalLikes() + 1;
+                    mNewsDbRef.child(news.getPostId()).child("totalLikes").setValue(totalLikes);
+
+
+                    Drawable img = context.getResources().getDrawable(R.drawable.ic_thumb_up_blue_24dp);
+                    btnLike.setCompoundDrawablesWithIntrinsicBounds(img, null, null, null);
+                    btnLike.setText("Liked");
+                    btnLike.setTextColor(Color.BLUE);
+
+
+                }
+            });
+        }
+
     }
 }
--- ItNewsApp/app/src/main/java/app/muneef/itnewsapp/fragments/NewsFragment.java
+++ ItNewsApp/app/src/main/java/app/muneef/itnewsapp/fragments/NewsFragment.java
@@ -4,16 +4,31 @@ package app.muneef.itnewsapp.fragments;
 import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import app.muneef.itnewsapp.R;
 import app.muneef.itnewsapp.activities.WriteNewsActivity;
+import app.muneef.itnewsapp.adapters.NewsApapter;
+import app.muneef.itnewsapp.models.News;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -21,6 +36,13 @@ import app.muneef.itnewsapp.activities.WriteNewsActivity;
 public class NewsFragment extends Fragment {
 
     FloatingActionButton floatingAction_news;
+    RecyclerView RvNews;
+    NewsApapter newsApapter;
+    List<News> newsList;
+
+    DatabaseReference mNewsDbRef;
+    ChildEventListener mNewsListener;
+    ValueEventListener mNewsValuesListener;
 
 
     public NewsFragment() {
@@ -35,6 +57,44 @@ public class NewsFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_news, container, false);
         floatingAction_news = view.findViewById(R.id.floatingAction_news);
 
+        RvNews = view.findViewById(R.id.RvNews);
+
+        mNewsDbRef = FirebaseDatabase.getInstance().getReference().child("News");
+
+        RvNews.setLayoutManager(new LinearLayoutManager(getContext()));
+        RvNews.setHasFixedSize(true);
+        newsList = new ArrayList<>();
+        fillRecycler();
+        newsApapter = new NewsApapter(newsList, getContext());
+        RvNews.setAdapter(newsApapter);
+
+        mNewsValuesListener = new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                newsList.clear();
+                newsApapter.notifyDataSetChanged();
+
+
+                for (DataSnapshot valueRes : dataSnapshot.getChildren()) {
+                    News news = valueRes.getValue(News.class);
+                    newsList.add(news);
+                }
+
+
+                newsApapter = new NewsApapter(newsList, getContext());
+                RvNews.setAdapter(newsApapter);
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        };
+
+        mNewsDbRef.addValueEventListener(mNewsValuesListener);
+
         floatingAction_news.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -45,9 +105,58 @@ public class NewsFragment extends Fragment {
         });
 
 
-
-
         return view;
+    }
+
+
+    private void fillRecycler() {
+        mNewsListener = new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+                // DataSnapshot dataSnapshotRes = dataSnapshot.child("Statuses");
+
+                for (DataSnapshot valueRes : dataSnapshot.getChildren()) {
+//                        Status status = valueRes.getValue(Status.class);
+//                        statusList.add(status);
+                }
+
+
+                newsApapter = new NewsApapter(newsList, getContext());
+
+                RvNews.setAdapter(newsApapter);
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        };
+
+
+    }
+
+    public void notifiyChangeFromFrag() {
+        newsApapter = new NewsApapter(newsList, getContext());
+//        recStatusList.setAdapter(adapter);
+
+        fillRecycler();
+
     }
 
 }
